b72073bf3da2bf15ebaf1701b32438a8a5f3d5f1
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-02-29 07:02:03 +0530

    event handling ex 1

--- external/two/body.js
+++
@@ -1,8 +0,0 @@
-var v1 = prompt("Enter 1st number","");
-var v2 = prompt("Enter 2nd number", "");
-
-var a = parseInt(v1);
-var b = parseInt(v2);
-
-var m = Math.max(a,b);
-document.write("The maximum is = " + m);
---
+++ external/two/head.js
@@ -0,0 +1,6 @@
+function mouseover(){
+    document.bgColor = "red";
+}
+function mouseout(){
+    document.bgColor = "yellow";
+}
